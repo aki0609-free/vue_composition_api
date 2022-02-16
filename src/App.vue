76be1b76f7c97078becdbcb648018449f5322dc5
aki0609-free <akiyamafree0609@gmail.com>
2022-02-16 23:09:29 +0900

    init

--- src/App.vue
+++ src/App.vue
@@ -1,15 +1,47 @@
 <template>
-  <section class="container">
-    <h2>{{ userName }}</h2>
-  </section>
+  <div>
+    <the-header></the-header>
+    <router-view></router-view>
+  </div>
 </template>
 
 <script>
+import { ref, provide } from 'vue';
+
+import TheHeader from './components/TheHeader.vue';
+
 export default {
-  data() {
-    return {
-      userName: 'Maximilian',
+  components: {
+    TheHeader,
+  },
+  setup() {
+    const products = ref([
+      {
+        id: 'p1',
+        title: 'A Carpet',
+        description: 'A nice looking, maybe a little bit used carpet.',
+        price: 15.99,
+      },
+      {
+        id: 'p2',
+        title: 'A Book',
+        description: 'You can read it. Maybe you should read it.',
+        price: 12.99,
+      },
+    ]);
+
+    const addProduct = (productData) => {
+      const newProduct = {
+        id: new Date().toISOString(),
+        title: productData.title,
+        description: productData.description,
+        price: productData.price,
+      };
+      products.value.push(newProduct);
     };
+
+    provide('products', products);
+    provide('addProduct', addProduct);
   },
 };
 </script>
